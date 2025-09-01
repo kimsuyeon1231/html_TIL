bfd9f534f9b1aa4bfb4e9e9d8991a49fe2deda2e
kimsuyeon1231 <a01099186626@gmail.com>
2025-09-01 09:49:55 +0900

    20250901

---
+++ figma_layout/samlip/footer.php
@@ -0,0 +1,39 @@
+<footer>
+            <div class="fr_top">
+                <div class="menu">
+                    <a href="#">개인정보처리방침</a>
+                    <a href="#">영상정보처리기기 운영관리방침</a>
+                    <a href="#">거래희망회사 사전등록</a>
+                    <a href="#">동반성장</a>
+                    <a href="#"><img src="./images/ft.ccm.png" alt="ccm"></a>
+                </div>
+                <ul class="site_all">
+                    <li><p>FAMLY SITES</p><!-- 화살표 bg -->
+                        <ul class="family_list">
+                            <li><a href="#">FAMLY SITES</a></li>
+                            <li><a href="#">Paris Baguette</a></li>
+                            <li><a href="#">Baskin Robbins</a></li>
+                            <li><a href="#">Dunkin'</a></li>
+                            <li><a href="#">Paris Croissant</a></li>
+                            <li><a href="#">Shake Shack</a></li>
+                            <li><a href="#">Caffè Pascucci</a></li>
+                            <li><a href="#">Jamba Juice</a></li>
+                            <li><a href="#">Happy Point</a></li>
+                            <li><a href="#">Lagrillia</a></li>
+                        </ul>
+                    </li>
+                    <li><p>SITE MAP</p>
+                        <div class="sitemap_contents">
+                            <h2>Sitemap</h2>
+                            <button type="button" class="close">X</button>
+                            <!-- 사이트맵 목록 작성위치 -->
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="fr_btm">
+                <address><em>Customer Service</em>고객센터 전화번호 080-739-8572 (월~금 09:00~17:00)<br>
+                    101, Gongdan 1-daero, Siheung-si, Gyeonggi-do, Republic of Korea ㈜SPC삼립 대표이사 황종현</address>
+                <p>&copy; SPC Samlip. All rights reserved.</p>
+            </div>
+        </footer>
